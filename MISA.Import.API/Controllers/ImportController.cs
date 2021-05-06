e64da898072bad9ac3de9d5988d7768a057b2628
DUNG TA DINH <tddung@rd.local>
2021-05-06 14:22:19 +0700

    simple code 1

--- MISA.Import.API/Controllers/ImportController.cs
+++ MISA.Import.API/Controllers/ImportController.cs
@@ -88,89 +88,9 @@ namespace MISA.Import.API.Controllers
                 return customerGroupid;
             }
         }
-        [HttpPost]
-        public async Task<List<Customer>> Import(IFormFile formFile, CancellationToken cancellationToken)
+
+        private async void GetDataFromFile(IFormFile formFile, CancellationToken cancellationToken, List<Customer> list)
         {
-            if (formFile == null || formFile.Length <= 0)
-            {
-                throw new BadRequestException("formfile is empty");
-            }
-
-            if (!Path.GetExtension(formFile.FileName).Equals(".xlsx", StringComparison.OrdinalIgnoreCase))
-            {
-                throw new BadRequestException("Not Support file extension");
-            }
-
-            var list = new List<Customer>();
-
-            using (var stream = new MemoryStream())
-            {
-                await formFile.CopyToAsync(stream, cancellationToken);
-
-                using (var package = new ExcelPackage(stream))
-                {
-                    ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
-                    var rowCount = worksheet.Dimension.Rows;
-
-                    for (int row = 2; row <= rowCount; row++)
-                    {
-                        list.Add(new Customer
-                        {
-                            CustomerCode = checkString(worksheet.Cells[row, 1].Value),
-                            Fullname = checkString(worksheet.Cells[row, 2].Value),
-                            MemberCardCode = checkString(worksheet.Cells[row, 3].Value),
-                            CustomerGroupId = GetCustomerGroupIdByName(Encoding.Default.GetBytes(checkString(worksheet.Cells[row, 4].Value))),
-                            PhoneNumber = checkString(worksheet.Cells[row, 5].Value),
-                            DateOfBirth = checkDateTime(worksheet.Cells[row, 6].Value),
-                            CompanyName = checkString(worksheet.Cells[row, 7].Value),
-                            TaxCode = checkString(worksheet.Cells[row, 8].Value),
-                            Email = checkString(worksheet.Cells[row, 9].Value),
-                            Address = checkString(worksheet.Cells[row, 10].Value),
-                            Note = checkString(worksheet.Cells[row, 11].Value),
-                        });
-                    }
-                }
-            }
-
-            //check customer
-
-            for (int i = 0; i < list.Count; i++)
-            {
-                    for (int j = 0; j < i; j++)
-                {
-                    if (list[j].CustomerCode.Equals(list[i].CustomerCode)) list[i].Status += "Mã khách hàng đã trùng với khách hàng khác trong tệp nhập khẩu.";
-                    if (list[j].PhoneNumber.Equals(list[i].PhoneNumber)) list[i].Status += "SĐT đã trùng với SĐT của khách hàng khác trong tệp nhập khẩu.";
-                }
-
-                if (CheckCustomerCode(list[i].CustomerCode)) list[i].Status += "Mã khách hàng đã tồn tại trong hệ thống.";
-                if (CheckPhoneNumber(list[i].CustomerCode)) list[i].Status += "SĐT đã có trong hệ thống.";
-                if (list[i].CustomerGroupId == null) list[i].Status += "Nhóm khách hàng không có trong hệ thống.";
-            }
-            for (int i = 0; i < list.Count; i++)
-            {
-                if (list[i].Status == null) list[i].Status = "Hợp lệ.";
-            }
-
-                return list;
-
-        }
-
-
-
-        [HttpPost("AddToDb")]
-        public async Task<int> ImportToDb(IFormFile formFile, CancellationToken cancellationToken)
-        {
-            if (formFile == null || formFile.Length <= 0)
-            {
-                throw new BadRequestException("formfile is empty");
-            }
-
-            if (!Path.GetExtension(formFile.FileName).Equals(".xlsx", StringComparison.OrdinalIgnoreCase))
-            {
-                throw new BadRequestException("Not Support file extension");
-            }
-
-            var list = new List<Customer>();
 
             using (var stream = new MemoryStream())
             {
@@ -217,18 +137,58 @@ namespace MISA.Import.API.Controllers
             }
             for (int i = 0; i < list.Count; i++)
             {
+                if (list[i].Status == null) list[i].Status = "Hợp lệ.";
+            }
+        }
+        [HttpPost]
+        public List<Customer> Import(IFormFile formFile, CancellationToken cancellationToken)
+        {
+            if (formFile == null || formFile.Length <= 0)
+            {
+                throw new BadRequestException("formfile is empty");
+            }
+
+            if (!Path.GetExtension(formFile.FileName).Equals(".xlsx", StringComparison.OrdinalIgnoreCase))
+            {
+                throw new BadRequestException("Not Support file extension");
+            }
+            var list = new List<Customer>();
+            GetDataFromFile(formFile, cancellationToken, list);
+            return list;
+        }
+
+
+
+        [HttpPost("AddToDb")]
+        public int ImportToDb(IFormFile formFile, CancellationToken cancellationToken)
+        {
+            if (formFile == null || formFile.Length <= 0)
+            {
+                throw new BadRequestException("formfile is empty");
+            }
+
+            if (!Path.GetExtension(formFile.FileName).Equals(".xlsx", StringComparison.OrdinalIgnoreCase))
+            {
+                throw new BadRequestException("Not Support file extension");
+            }
+
+            var list = new List<Customer>();
+            var rowsAffect = 0;
+            GetDataFromFile(formFile, cancellationToken, list);
+            for (int i = 0; i < list.Count; i++)
+            {
                 DynamicParameters dynamicParameters = new DynamicParameters();
-                if (list[i].Status == null)
+                if (list[i].Status == "Hợp lệ.")
                 {
                     dynamicParameters.Add("@Customer", list[i]);
                     using (dbConnection = new MySqlConnection(connectionString))
                     {
-                        var rowsAffect = dbConnection.Execute("InsertCustomer", param: list[i], commandType: CommandType.StoredProcedure);
+                         rowsAffect = dbConnection.Execute("InsertCustomer", param: list[i], commandType: CommandType.StoredProcedure);
+                        if (rowsAffect != 1) return rowsAffect;
                     }
-                    
                 }
             }
-            return 1;
+            return rowsAffect;
         }
     }
 }
